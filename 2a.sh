2851a48af8a9e3f0ff1c044ffe9dc6dc8fe7625e
Sameeksha07-pura <98949863+Sameeksha07-pura@users.noreply.github.com>
2022-02-24 14:35:46 +0530

    lab prg2

--- 2a.sh
+++ 2a.sh
@@ -1,4 +1,4 @@
-#2a. Write a shell script that accepts two filenames as arguments, checks if the permissions for
+# 2a. Write a shell script that accepts two filenames as arguments, checks if the permissions for
 #these files are identical and if the permissions are identical, output common permissions
 #otherwise output each filename followed by its permissions.
 echo -n "Enter the first file name"
