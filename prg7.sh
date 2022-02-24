87b3531a0a7dd2a6d7eae8324d2be7f946be6628
Sameeksha07-pura <98949863+Sameeksha07-pura@users.noreply.github.com>
2022-02-24 14:28:34 +0530

    lab prg7

--- prg7.sh
+++ prg7.sh
@@ -1,5 +1,5 @@
 #7a. Write a shell script that gets executed and displays the message either “Good Morning” or
-“Good Afternoon” or “Good Evening” depending upon time at which the user logs in.
+#“Good Afternoon” or “Good Evening” depending upon time at which the user logs in.
 #h=`date +%H`
 ls -l
 h=$1
