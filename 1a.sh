22938a12b157a9b0d0c4d9f4b6e81403e711c1be
Sameeksha07-pura <98949863+Sameeksha07-pura@users.noreply.github.com>
2022-04-06 16:50:27 +0530

    Add files via upload

--- 1a.sh
+++ 1a.sh
@@ -1,15 +1,15 @@
 if [ $# -eq 1 ]
 then
-if [ -d $1 ]
-then
-set -- `ls -Rl $1 | tr -s " " | grep "^-" | cut -d " " -f 5,9- | sort -n | tail -n 1`
-echo "file name is $2"
-echo "File size is $1"
-exit
+	if [ -d $1 ]
+	then
+		set -- `ls -Rl $1 | tr -s " " | grep "^-" | cut -d " " -f 5,9- | sort -n | tail -n 1`
+		echo "file name is $2"
+		echo "File size is $1"
+		exit
+	else
+		echo "Not a directory"
+		exit
+	fi
 else
-echo "Not a directory"
-exit
-fi
-else
-echo "Enter a directory name"
+	echo "Enter a directory name"
 fi
